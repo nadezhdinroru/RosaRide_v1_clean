fb2e54399124131904d306aafc1980e5afb7fcb3
Nikolay NikSoN <nikson@example.com>
2025-11-18 11:46:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-book").onclick = () => {
+  alert("Переход к бронированию");
+};
+
+document.getElementById("btn-rent").onclick = () => {
+  alert("Переход к аренде");
+};
+
+document.getElementById("btn-instructors").onclick = () => {
+  alert("Переход к списку инструкторов");
+};
