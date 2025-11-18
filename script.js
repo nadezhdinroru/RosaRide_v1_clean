0ec9a56953e6a8b7c6b240aa40798aea3ce13173
Nikolay NikSoN <nikson@example.com>
2025-11-18 13:33:34 +0300

    test hook

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ document.getElementById("btn-instructors").onclick = () => {
   alert("Переход к списку инструкторов");
 };
 console.log("test");
+const a = 5;
+console.log(a);
+
+
+console.log("hook test")
+
+console.log("hook test 123");
