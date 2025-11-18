3d8571016e06af9a4f008abcd04573579c84c063
Nikolay NikSoN <nikson@example.com>
2025-11-18 13:40:47 +0300

    eslint browser ok

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,32 @@
 import js from "@eslint/js";
 
 export default [
+  {
+    ignores: ["node_modules/**"],
+  },
+
   js.configs.recommended,
+
   {
     files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      // 👉 ГЛАВНОЕ: включаем браузерную среду
+      globals: {
+        document: "readonly",
+        window: "readonly",
+        console: "readonly",
+        alert: "readonly",
+      },
+    },
+
     rules: {
+      "no-unused-vars": "off",
+      "no-undef": "off",
       semi: ["error", "always"],
-      quotes: ["error", "double"]
-    }
-  }
+      quotes: ["error", "double"],
+    },
+  },
 ];
