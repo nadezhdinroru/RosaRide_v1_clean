915705847a9e2810522e9195b3e6fc0577a91a56
Nikolay NikSoN <nikson@example.com>
2025-11-18 13:39:42 +0300

    eslint final test

--- eslint.config.js
+++ eslint.config.js
@@ -4,13 +4,7 @@ export default [
   js.configs.recommended,
   {
     files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module"
-    },
     rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
       semi: ["error", "always"],
       quotes: ["error", "double"]
     }
